41b11791c646720c4f0204529966696fa8df4b96
alek żur <p.zur@outlook.com>
2017-10-27 08:01:23 +0200

    Dodaj pliki projektów.

---
+++ MagazynProba1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagazynProba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
